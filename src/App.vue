09b74efdafbf19dc74d1c1df8497eb70f48689b4
ekeuulrich2 <ekeuulrich2@gmail.com>
2020-10-16 18:41:45 +0200

    Initial commit
    - Project setup
        - Created App.Vue file
        - Project structuring (creation of component folder)
    - Added some dummy data in App.vue
    - Created two component (LearningResource.vue and StoredResources.vue)
        - Added the template, props and some styling

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/learning-resources/StoredResources.vue';
+
+export default {
+  components: {
+    StoredResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-vue-guide',
+          title: 'Official Vue Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'official-react-guide',
+          title: 'Official React Guide',
+          description: 'The official React.js documentation',
+          link: 'https://reactjs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn how to google...',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=B612:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'B612', 'sans-serif';
+}
+
+body {
+  margin: 0;
+}
+</style>
