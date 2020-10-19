2a7ea5aa1afb079769e14a70f14ed391593a0969
ekeuulrich2 <ekeuulrich2@gmail.com>
2020-10-19 21:45:32 +0200

    Feat - Dynamic components  - Added a dynamic component to switch between stored resources and adding a new resource

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <the-header title="Resource App"></the-header>
-  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue';
 import TheHeader from './components/layouts/TheHeader.vue'
 
 export default {
   components: {
-    StoredResources,
     TheHeader,
   },
   data() {
---
+++ src/components/learning-resources/TheResource.vue
@@ -0,0 +1,29 @@
+<template>
+  <base-card>
+    <base-button @click="setSectedTab('stored-resources')">Stored Resources</base-button>
+    <base-button @click="setSectedTab('add-resource')">Add Resource</base-button>
+  </base-card>
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue'
+
+export default {
+  components: {
+    StoredResources,
+    AddResource
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+    };
+  },
+  methods: {
+    setSectedTab(tab) {
+        this.selectedTab = tab;
+    } 
+  }
+};
+</script>
